665cb5b256e075b40fe3c690b70284832ff8d287
Lordedgar03 <148795435+Lordedgar03@users.noreply.github.com>
2025-06-06 12:17:17 +0000

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -5,3 +5,7 @@ const serviceAccount = JSON.parse(process.env.FIREBASE_KEY_JSON);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
+
+const db = admin.firestore(); // 🔥 Firestore
+
+module.exports = db;
